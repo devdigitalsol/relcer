97aba388eb1604f945cbc446762910ddcc9270d0
sachind3 <desai.sachin45@gmail.com>
2021-08-19 18:15:44 +0530

    wip

--- js/app.js
+++ js/app.js
@@ -2,6 +2,64 @@ const loader = document.querySelector(".loader");
 const screen1 = document.querySelector(".screen-1");
 const screen2 = document.querySelector(".screen-2");
 const cover = document.querySelector(".cover");
+
+const data = [
+  {
+    id: 0,
+    question:
+      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
+    option: [
+      "Aluminium hydroxide",
+      "Deglycyrrhizinated Liquorice",
+      "Magnesium Hydroxide",
+    ],
+    correct: "Aluminium hydroxide",
+  },
+  {
+    id: 1,
+    question:
+      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
+    option: [
+      "Aluminium hydroxide",
+      "Deglycyrrhizinated Liquorice",
+      "Magnesium Hydroxide",
+    ],
+    correct: "Aluminium hydroxide",
+  },
+  {
+    id: 2,
+    question:
+      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
+    option: [
+      "Aluminium hydroxide",
+      "Deglycyrrhizinated Liquorice",
+      "Magnesium Hydroxide",
+    ],
+    correct: "Aluminium hydroxide",
+  },
+  {
+    id: 3,
+    question:
+      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
+    option: [
+      "Aluminium hydroxide",
+      "Deglycyrrhizinated Liquorice",
+      "Magnesium Hydroxide",
+    ],
+    correct: "Aluminium hydroxide",
+  },
+  {
+    id: 4,
+    question:
+      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
+    option: [
+      "Aluminium hydroxide",
+      "Deglycyrrhizinated Liquorice",
+      "Magnesium Hydroxide",
+    ],
+    correct: "Aluminium hydroxide",
+  },
+];
 window.addEventListener("load", (event) => {
   loader.remove();
   screen1.classList.add("active");
