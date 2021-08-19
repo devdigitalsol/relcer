d702ef308a8dfbc9ccf432978fb90d0ab2a989fe
sachind3 <desai.sachin45@gmail.com>
2021-08-19 19:58:55 +0530

    wip

--- js/app.js
+++ js/app.js
@@ -2,8 +2,13 @@ const loader = document.querySelector(".loader");
 const screen1 = document.querySelector(".screen-1");
 const screen2 = document.querySelector(".screen-2");
 const cover = document.querySelector(".cover");
-
-const data = [
+const quiz = document.querySelector(".quiz");
+const quizOptions = document.querySelector(".quizOptions");
+const scoreCount = document.querySelector("#scoreCount");
+const playBtn = document.getElementById("playBtn");
+let currentQuestion = 0;
+let score = 0;
+const mydata = [
   {
     id: 0,
     question:
@@ -13,51 +18,37 @@ const data = [
       "Deglycyrrhizinated Liquorice",
       "Magnesium Hydroxide",
     ],
-    correct: "Aluminium hydroxide",
+    correct: "Deglycyrrhizinated Liquorice",
   },
   {
     id: 1,
-    question:
-      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
-    option: [
-      "Aluminium hydroxide",
-      "Deglycyrrhizinated Liquorice",
-      "Magnesium Hydroxide",
-    ],
-    correct: "Aluminium hydroxide",
+    question: "______ in Relcer helps in faster elimination of Gas.",
+    option: ["Simethicone", "Deglycyrrhizinated Liquorice"],
+    correct: "Simethicone",
   },
   {
     id: 2,
-    question:
-      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
-    option: [
-      "Aluminium hydroxide",
-      "Deglycyrrhizinated Liquorice",
-      "Magnesium Hydroxide",
-    ],
-    correct: "Aluminium hydroxide",
+    question: "Relcer bans the burn in stomach and ensures freedom from",
+    option: ["Bloating", "Burping", "Flatulence", "All of the above"],
+    correct: "All of the above",
   },
   {
     id: 3,
-    question:
-      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
+    question: "Which of the following is not a feature of Relcer",
     option: [
-      "Aluminium hydroxide",
-      "Deglycyrrhizinated Liquorice",
-      "Magnesium Hydroxide",
+      "Long shelf Life (4yrs)",
+      "Very stable suspension",
+      "Fast acid neutralization",
+      "Comes in a Glass bottle",
     ],
-    correct: "Aluminium hydroxide",
+    correct: "Comes in a Glass bottle",
   },
   {
     id: 4,
     question:
-      "Relcer has ___________component which ensures anti inflammatory actions leading to mucosal healing.",
-    option: [
-      "Aluminium hydroxide",
-      "Deglycyrrhizinated Liquorice",
-      "Magnesium Hydroxide",
-    ],
-    correct: "Aluminium hydroxide",
+      "Relcer ensures – Rapid Relief, Reflux prevention and Rebuilds lining.",
+    option: ["True", "False"],
+    correct: "True",
   },
 ];
 window.addEventListener("load", (event) => {
@@ -76,13 +67,64 @@ window.addEventListener("load", (event) => {
   }, 2000);
 });
 
-const playBtn = document.getElementById("playBtn");
-
 playBtn.addEventListener("click", () => {
   document.querySelector(".revealBlock").classList.add("show");
   setTimeout(() => {
     screen1.classList.remove("active");
     screen2.classList.add("active");
+    mytimer();
   }, 200);
   cover.classList.add("active");
 });
+const yellowPatch = document.getElementById("yellowPatch");
+
+function mytimer() {
+  let count = 60;
+  timer = setInterval(function () {
+    document.getElementById("timerCount").textContent = count--;
+    if (count == 1) clearInterval(timer);
+  }, 1000);
+}
+
+function startQuiz() {
+  let newQuestion;
+  function quiz() {
+    scoreCount.textContent = `${score}/${mydata.length}`;
+    if (currentQuestion <= mydata.length - 1) {
+      newQuestion = mydata[currentQuestion].question;
+      yellowPatch.innerHTML = `<span>${newQuestion}</span>
+                <img src="images/yellowPatch.png" />`;
+      let newOptions = [];
+      mydata[currentQuestion].option.forEach((item) => {
+        newOptions.push(
+          `<button class='option' data-option='${item}'><span>${item}</span><img src='images/bluePatch.png'></button>`
+        );
+      });
+      quizOptions.innerHTML = newOptions.join("");
+    } else {
+      console.log("game over");
+      clearInterval(timer);
+    }
+    document.querySelectorAll(".option").forEach((item) => {
+      item.addEventListener("click", () => {
+        if (currentQuestion <= mydata.length - 1) {
+          if (
+            item.getAttribute("data-option") === mydata[currentQuestion].correct
+          ) {
+            score = score + 1;
+            console.log("correct ans");
+            scoreCount.textContent = `${score}/${mydata.length}`;
+          } else {
+            console.log("wrong ans");
+            scoreCount.textContent = `${score}/${mydata.length}`;
+          }
+          currentQuestion = currentQuestion + 1;
+          console.log(score, currentQuestion);
+          quiz();
+        }
+      });
+    });
+  }
+  quiz();
+}
+startQuiz();
